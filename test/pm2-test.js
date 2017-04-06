d39c4e9b9c0c6b9664f8e47295991045bedd09b4
stojnovsky@gmail.com <stojnovsky@gmail.com>
2017-04-06 14:42:41 +0300

    graphql auth & events log

--- test/pm2-test.js
+++ test/pm2-test.js
@@ -5,23 +5,10 @@ const pm2 = require('pm2');
 
 
 
-pm2.connect(function(err){
-  if (err) {
-    console.error(err);
-    process.exit(2);
-  }
-
-  pm2.list(function(err, processes){
-    processes.forEach(function(proc){
-      console.log(proc.name, '-->', proc.pm2_env.status);
-    })
-  })
-
-})
-
-
 pm2.launchBus(function(err, bus) {
-  bus.on('log:out', function(data){
-    console.log(data);
+  bus.on('process:event', function(process_event){
+    console.log(process_event.event,
+      process_event.process.pm_id,
+      process_event.process.name);
   });
 });
